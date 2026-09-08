0b3c78a6fa449e5fb4cac717b3a7dd8109292d8b
JKZ-MILLER <juancarlosdasilva2021@gmail.com>
2026-09-08 00:56:39 -0300

    Add files via upload

--- admin/config.js
+++ admin/config.js
@@ -1,1 +1,4 @@
-window.VJ_SUPABASE = { url: "", anonKey: "" };
+window.VJ_SUPABASE = {
+  url: "https://dciunhrttojsctdlmfvj.supabase.co",
+  anonKey: "sb_publishable_pH3oQXt64KP-aN9hCqaFrQ_kZ1L6lM8"
+};
